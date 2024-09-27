0fea949dc943afe4b83232f748e2d1f5b17747c8
pritam16 <pritammajumder416@gmail.com>
2024-09-27 12:12:16 +0530

    signup

--- src/controllers/auth/signup/index.ts
+++ src/controllers/auth/signup/index.ts
@@ -1,12 +1,37 @@
 import Router, { NextFunction, Request, Response } from "express";
+import { successResponse } from "../../../utils/response";
+import User from "../../../models/user";
+import { HttpException } from "../../../classes/HttpException";
 
 const router = Router();
 
-router.post("/", (request: Request, response: Response, next: NextFunction) => {
-  try {
-  } catch (err) {
-    next(err);
+router.post(
+  "/",
+  async (request: Request, response: Response, next: NextFunction) => {
+    const { username, email, password } = request.body;
+
+    if (!username || !email || !password) {
+      return next(new HttpException(400, "All fields are required"));
+    }
+
+    try {
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        throw new HttpException(409, "User already exists");
+      }
+
+      const newUser = new User({ username, email, password });
+      await newUser.save();
+
+      successResponse({
+        message: "User created successfully",
+        data: { user: { username, email } },
+        res: response,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 export default router;
